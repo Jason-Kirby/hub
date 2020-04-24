359ea0fa465823fed59f5999f8925a4daf563341
Jason Kirby <jason@jasonkirby.com>
2020-04-24 14:53:46 -0700

    favicon failure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
 
-
-import Header from "./components/Header";
-import Landing from "./css/Landing";
-import './sass/_base.scss';
 
 
 class App extends Component {
@@ -13,13 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="container" >
-        <BrowserRouter>
-          <div>
-            <Header />
-            <Route exact path="/" component={Landing} />
-
-          </div>
-        </BrowserRouter>
+       
       </div>
     );
   }
